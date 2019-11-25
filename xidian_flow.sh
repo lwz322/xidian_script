266b36708080c7b6e2974af0c0c80beac63c1069
lwz322 <lwz322@qq.com>
2019-11-25 08:40:05 +0800

    fix sh

--- xidian_flow.sh
+++ xidian_flow.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 log_file="./xidian_flow.log"
+script_path="/root/xidian_me_log.py"
 [ -f $log_file ] && pre_value=$(tail -1 $log_file | awk '{print $6}')
-query_output=$(python3 ./xidian_flow_log.py 2>/dev/null)
+query_output=$(python3 $script_path 2>/dev/null)
 query_value=$(echo $query_output | awk '{print $6}')
 [ "$pre_value" != "$query_value" ] || exit 0
 echo $query_output>> $log_file
--- xidian_me.sh
+++ xidian_me.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-log_file="./xidian_me.log"
+log_file="/root/xidian_me.log"
+script_path="/root/xidian_me_log.py"
 [ -f $log_file ] && pre_value=$(tail -1 $log_file | awk '{print $6}')
-query_output=$(python3 ./xidian_me_log.py 2>/dev/null)
+query_output=$(python3 $script_path 2>/dev/null)
 query_value=$(echo $query_output | awk '{print $6}')
 [ "$pre_value" != "$query_value" ] || exit 0
-[ -z $pre_value ] && useage=`echo $pre_value-$query_value | bc`
+[ -n $pre_value ] && useage=`echo $pre_value-$query_value | bc`
 echo $query_output $useage >> $log_file
