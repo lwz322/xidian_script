49ff4100b7524a7003003f8a5842011f265c7c86
lwz322 <lwz322@qq.com>
2020-06-20 10:26:39 +0800

    fix condition

--- xidian_me.sh
+++ xidian_me.sh
@@ -4,5 +4,5 @@ log_file="./xidian_me.log"
 query_output=$(python3 ./xidian_me_log.py 2>/dev/null)
 query_value=$(echo $query_output | awk '{print $6}')
 [ $pre_value != $query_value ] || exit 0
-[ -z $pre_value ] && useage=`echo $pre_value-$query_value | bc`
+[ -n $pre_value ] && useage=`echo $pre_value-$query_value | bc`
 echo $query_output $useage >> $log_file
